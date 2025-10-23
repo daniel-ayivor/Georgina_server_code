fb6e3cd8f6514b5cbbc8c26fbc126f11e024a6d3
daniel-ayivor <danielkofiayivor23@gmail.com>
2025-10-23 17:16:43 +0000

    db connection

--- Database/database.js
+++ Database/database.js
@@ -1,27 +1,20 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Needed for Supabase SSL
-      },
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    logging: false, // Optional: turn off SQL query logs
-  }
-);
+  },
+  logging: false,
+});
 
 sequelize
   .authenticate()
-  .then(() => console.log("✅ Connected to Supabase PostgreSQL successfully!"))
+  .then(() => console.log("✅ Connected to PostgreSQL successfully!"))
   .catch((err) => console.error("❌ Database connection error:", err));
 
 module.exports = sequelize;

5b9b4d14a76a1de587a6c69da96c3e37c7b407a4
daniel-ayivor <danielkofiayivor23@gmail.com>
2025-11-13 15:38:01 +0000

    loading component

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,28 +1,52 @@
+
 const { DataTypes } = require("sequelize");
 const sequelize = require("../Database/database");
 
-const User = sequelize.define(
-  "User",
-  {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING, allowNull: false },
-    email: {
-      type: DataTypes.STRING(150),
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: { msg: "Please enter a valid email" },
-        len: { args: [5, 150], msg: "Email must be between 5 and 150 characters" },
-      },
+const User = sequelize.define("User", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
     },
-    password: { type: DataTypes.STRING, allowNull: true },
-    contact: { type: DataTypes.STRING, allowNull: true },
-    role: { type: DataTypes.ENUM("user", "admin"), allowNull: false, defaultValue: "user" },
-  },
-  {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(150), 
+        allowNull: false,
+        validate: {
+            isEmail: {
+                msg: 'Please enter a valid email'
+            },
+            len: {
+                args: [5, 150], 
+                msg: 'Email must be between 5 to 150 characters'
+            }
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: true // Change to true temporarily
+    },
+    contact: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    role: {
+        type: DataTypes.ENUM("user", "admin"),
+        allowNull: false,
+        defaultValue: "user"
+    }
+}, {
     tableName: "users",
-    indexes: [{ unique: true, fields: ["email"], name: "unique_email_index" }],
-  }
-);
+    indexes: [
+        {
+            unique: true,
+            fields: ['email'],
+            name: 'unique_email_index'
+        }
+    ]
+});
 
 module.exports = User;
